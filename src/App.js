5a44f52d5d1b2190f6122732d149e19d6a0a6a72
Roma Khajiev <raxmon1710@gmail.com>
2023-03-17 00:35:46 +0800

    Initial message

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+  console.log("This is a console inside App")
+  const show_employees = true;
+  return (
+    <div className="App">
+
+
+    </div>
+  );
+}
+
+export default App;
