d5765b81214f96cb5cab463bab0274f35f32660a
Roma Khajiev <raxmon1710@gmail.com>
2023-03-24 16:15:40 +0800

    Updating info done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [employees, set_employees] = useState([
     {
+      id: 1,
       name: "Roma",
       role: "Developer",
       img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
     },
     {
+      id: 2,
       name: "Mohamed",
       role: "Marketmaker",
       img: "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?",
     },
     {
+      id: 3,
       name: "Hans",
       role: "CEO",
       img: "https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg",
     },
     {
+      id: 4,
       name: "Tapiwa",
       role: "Manager",
       img: "https://images.pexels.com/photos/4153800/pexels-photo-4153800.jpeg",
     },
   ]);
+
+  function updateEmployee(id, new_name, new_role) {
+    const updated_employees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: new_name, role: new_role };
+      }
+
+      return employee;
+    });
+    set_employees(updated_employees);
+  }
 
   return (
     <div className="App">
@@ -33,10 +47,12 @@ function App() {
         {employees.map((employee) => {
           return (
             <Employee
-              key={uuidv4()} //Generate a random key
+              key={employee.id}
+              id={employee.id}
               name={employee.name}
               role={employee.role}
               img={employee.img}
+              updateEmployee={updateEmployee}
             />
           );
         })}
