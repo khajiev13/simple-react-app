adda96d40abbaf95a56ef9dd55a2aa518ae2509b
Roma Khajiev <raxmon1710@gmail.com>
2023-03-24 12:11:34 +0800

    Done with basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './index.css';
+import Employee from './components/Employee'
 
 function App() {
 
   console.log("This is a console inside App")
 
   return (
-    <div className="App bg-red-300">
-      Hi this is react with tailwind
+    <div className="App">
+      <div className='flex flex-wrap mt-3 justify-center'>
+        <Employee name="Roma" role="CEO and Developer" img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg" />
+        <Employee name="Justin" role="CEO and Developer" img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg" />
+        <Employee name="Mohammed" role="CEO and Developer" img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg" />
+        <Employee name="Dinur" role="CEO and Developer" img="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg" />
+      </div>
+
 
     </div>
   );
